afc0800fbc7e31ebb5f596314c264ddba1dc6e2c
Hamad-Younas <hamadyounas191706@gmail.com>
2024-07-26 12:40:14 +0500

    configuration

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/stripe/payment', (req, res) => {
-  
-// });
+app.use('/', (req, res) => {
+  res.send("hello world")
+});
 app.use('/users', usersRouter);
 app.use("/data", dataRouter);
 app.use("/stripe", stripeRouter);
