0c7ede80d53178bc13b384262dc5c57d1d90c531
Hamad-Younas <hamadyounas191706@gmail.com>
2024-07-26 12:34:21 +0500

    configuration

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var app = express();
 app.use(cors());
 app.use(express.json());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
