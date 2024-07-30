9cbef9fe49d9384ca1b0821ac495843d11a0093a
Hamad-Younas <hamadyounas191706@gmail.com>
2024-07-30 13:24:07 +0500

    complete

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var app = express();
 app.use(cors());
 app.use(express.json());
 
-// // view engine setup
+// view engine setup (commented out because it's not used)
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
@@ -25,12 +25,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/users', usersRouter);
 app.use("/data", dataRouter);
 app.use("/stripe", stripeRouter);
 app.use('/', (req, res) => {
-  res.send({message:"Hello world"})
+  res.send({ message: "Hello world" });
 });
 
 // catch 404 and forward to error handler
@@ -38,19 +37,14 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // return the error as JSON
+  console.error(err.stack); // Log the error stack
   res.status(err.status || 500);
   res.json({
     message: err.message,
     error: req.app.get('env') === 'development' ? err : {}
   });
 });
-
-
 
 module.exports = app;
